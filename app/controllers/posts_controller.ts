3ed3193260d99e32dc10b606590135cc8b1165d5
Denny <denny.waris@kalbe.co.id>
2025-06-03 07:51:22 +0700

    feat: implement post creation functionality with validation and update form action

--- app/controllers/posts_controller.ts
+++ app/controllers/posts_controller.ts
@@ -1,5 +1,23 @@
+import Post from '#models/post'
+import { PostValidator } from '#validators/post'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class PostsController {
-  async store() {}
+  async store({ request, auth, session, response }: HttpContext) {
+    const payload = await request.validateUsing(PostValidator)
+
+    await Post.create({
+      userId: auth.user!.id,
+      content: payload.content,
+    })
+
+    session.flash({
+      notification: {
+        type: 'success',
+        message: 'Post created successfully.',
+      },
+    })
+
+    return response.redirect('/')
+  }
 }
